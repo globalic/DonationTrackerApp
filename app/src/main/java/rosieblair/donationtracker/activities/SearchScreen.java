c8b1689558fca23db59943cb03ba2c98b53eb889
Michelle Yang <michelleandschool@gmail.com>
2018-11-06 11:14:03 -0500

    Search screen

--- app/src/main/java/rosieblair/donationtracker/activities/SearchScreen.java
+++ app/src/main/java/rosieblair/donationtracker/activities/SearchScreen.java
@@ -1,15 +1,25 @@
 package rosieblair.donationtracker.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.content.Intent;
 
 import rosieblair.donationtracker.R;
 
 public class SearchScreen extends AppCompatActivity {
+
+    private EditText item;            // item input
+    private Spinner category;         // category spinner
+    private Spinner location;         // location spinner
+    private Button searchForItem;     // item search button
+    private Button searchForCat;      // category search button
+    //private Button searchForLoc;      // location search button
+    //private String catName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +28,46 @@ public class SearchScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        pressSearchItem();
+        pressSearchCat();
+        //pressSearchLoc();
+    }
+
+    public void pressSearchItem() {
+        location = (Spinner) findViewById(R.id.locSearchSpinner);
+        item = (EditText) findViewById(R.id.enterSearchItem);
+        searchForItem = (Button) findViewById(R.id.itemSearchButton);
+
+        searchForItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String itemName = item.getText().toString();
+                String locName = location.getSelectedItem().toString();
+                Intent intent = new Intent(getApplicationContext(), ItemsByName.class);
+                        //"edu.gatech.micheyang.pbjdonationtracker.ItemsByName");
+                intent.putExtra(ItemsByName.NAME, itemName);
+                intent.putExtra(ItemsByName.LOCATION, locName);
+                startActivity(intent);
             }
         });
     }
 
+    public void pressSearchCat() {
+        location = (Spinner) findViewById(R.id.locSearchSpinner);
+        category = (Spinner) findViewById(R.id.catSearchSpinner);
+        searchForCat = (Button) findViewById(R.id.catSearchButton);
+
+        searchForCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String catName = category.getSelectedItem().toString();
+                String locName = location.getSelectedItem().toString();
+                Intent intent = new Intent(getApplicationContext(), ItemsInCategory.class);
+                        //"edu.gatech.micheyang.pbjdonationtracker.ItemsInCategory");
+                intent.putExtra(ItemsInCategory.CATEGORY, catName);
+                intent.putExtra(ItemsInCategory.LOCATION, locName);
+                startActivity(intent);
+            }
+        });
+    }
 }
