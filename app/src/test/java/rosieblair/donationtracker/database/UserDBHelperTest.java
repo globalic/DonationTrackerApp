0ff5a6317d814498a5c84b6fa6ef484771ab59d9
Sabrina Wilson <sawilson3011@gmail.com>
2018-11-10 17:28:02 -0500

    Created UserDBHelperTest for checkUsername method

---
+++ app/src/test/java/rosieblair/donationtracker/database/UserDBHelperTest.java
@@ -0,0 +1,47 @@
+package rosieblair.donationtracker.database;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests checkUsername method in UserDBHelper class
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UserDBHelperTest {
+
+    private static final int TIMEOUT = 200;
+
+    private boolean testResult;
+    private UserDBHelper testDB = mock(UserDBHelper.class);
+
+
+    // Branch Coverage: username == null
+    // Expected: false
+    @Test (timeout = TIMEOUT)
+    public void testNullUsername() {
+        testResult = testDB.checkUsername("null");
+        assertFalse(testResult);
+    }
+
+    // Branch Coverage: username != null, exists
+    // Expected: count > 0 --> true
+    @Test (timeout = TIMEOUT)
+    public void testExistingUsername() {
+        when(testDB.checkUsername("existingName")).thenReturn(true);
+        testResult = testDB.checkUsername("existingName");
+        assertTrue(testResult);
+    }
+
+    // Branch Coverage: username != null, does not exist
+    // Expected: count > 0 --> false
+    @Test (timeout = TIMEOUT)
+    public void testNewUsername() {
+        when(testDB.checkUsername("newName")).thenReturn(false);
+        testResult = testDB.checkUsername("newName");
+        assertFalse(testResult);
+    }
+}
