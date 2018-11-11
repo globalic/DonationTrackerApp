d9d17ab4bacfb5d5efe168a8a1d684ff5a6bae16
rosie <rblair8@gatech.edu>
2018-11-11 16:13:27 -0500

    added location db helper junit test

---
+++ app/src/test/java/rosieblair/donationtracker/database/LocationDBHelperTest.java
@@ -0,0 +1,46 @@
+package rosieblair.donationtracker.database;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LocationDBHelperTest {
+
+    private static final int TIMEOUT = 200;
+
+    private boolean testResult;
+    private LocationDBHelper testDB = mock(LocationDBHelper.class);
+
+
+    // Branch Coverage: location name == null
+    // Expected: false
+    @Test(timeout = TIMEOUT)
+    public void testNullUsername() {
+        testResult = testDB.checkLocation("null");
+        assertFalse(testResult);
+    }
+
+    // Branch Coverage: location name != null, exists
+    // Expected: count > 0 --> true
+    @Test (timeout = TIMEOUT)
+    public void testExistingLocation() {
+        when(testDB.checkLocation("existingName")).thenReturn(true);
+        testResult = testDB.checkLocation("existingName");
+        assertTrue(testResult);
+    }
+
+    // Branch Coverage: location name != null, does not exist
+    // Expected: count > 0 --> false
+    @Test (timeout = TIMEOUT)
+    public void testNewLocation() {
+        when(testDB.checkLocation("newName")).thenReturn(false);
+        testResult = testDB.checkLocation("newName");
+        assertFalse(testResult);
+    }
+}
